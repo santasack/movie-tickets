f548d2baaaa358679ab7cabeb1ac9d168cd4c910
santasack <jackuknz@gmail.com>
2023-03-14 11:10:43 +1300

    update

---
+++ movie_tickets_v1.py
@@ -0,0 +1,8 @@
+            """Movie theatre ticketing system - v1
+            Welcome screen and set up variables
+            Created my Jack Lowe
+            """
+
+
+            def sell_ticket():
+                print("******** Fanfair Movies - ticketing system ********")
